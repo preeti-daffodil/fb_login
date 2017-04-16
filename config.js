4f600723cf27964bcbc52cfedc03bc866de6a540
preeti-start <preetigulia7@gmail.com>
2017-04-16 19:59:28 +0530

    url update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
     appID : "291008228003347",
     appSecreatKey : "adfaa35098875af96ee431f6a51f8c23",
-    redirectUrl : "http://localhost:3000/auth",
+    redirectUrl : "http://139.162.41.165:3000/auth",
     PORT:3000
 };
